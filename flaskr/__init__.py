4573f413b0028303a0751d80c49fb97dcf66f1bc
bailining <lb3224@stern.nyu.edu>
2019-06-29 16:27:57 -0400

    user database hook

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,25 @@
 from flask import Flask, jsonify
 from .book_request import get_categories, get_bestseller
+import os
+import pyrebase
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+dotenv_path = join(dirname(__file__), '.env')
+FIREBASE_API_KEY = os.getenv("FIREBASE_API_KEY")
+
+config = {
+  "apiKey": os.getenv('FIREBASE_API_KEY'),
+  "authDomain": "book-recommendation-engine.firebaseapp.com",
+  "databaseURL": "https://book-recommendation-engine.firebaseio.com",
+  "projectId": "book-recommendation-engine",
+  "storageBucket": "book-recommendation-engine.appspot.com",
+  "serviceAccount": "app/firebase-private-key.json",
+  "messagingSenderId": "1052538486568"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 app = Flask(__name__)
 
@@ -14,6 +34,11 @@ def bestseller(category):
 @app.route('/<name>')
 def hello_world(name):
    return 'Hello %s!' % name
+
+@app.route('/newbook')
+def add_book():
+	db.child("book").push({"book": "hello"})
+	return "Success"
 
 if __name__ == '__main__':
    app.run()
